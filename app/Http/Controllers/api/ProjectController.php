4c2b1e1a791c1fdb1bee4683b509eec87c69bc69
s4r44d43 <sararez@live.it>
2024-06-02 10:31:42 +0200

    [add] api with good endpoint, [tried successfully] thunder client call

--- app/Http/Controllers/api/ProjectController.php
+++ app/Http/Controllers/api/ProjectController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    // public function index(){
+    public function index(){
         
-    //     $posts = Project::all();
-    //     return response()->json($posts);
-    // }   API CON MODEL POST ESEMPIO PROGETTO PRECEDENTE
+        $project = Project::all();
+        return response()->json($project);
+    }   
 }
